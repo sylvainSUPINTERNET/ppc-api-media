ef47602ea0f6ded55ee2ae1ad9b7ae60606aa8ec
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-01-14 20:21:13 +0100

    [UPDATE] heatmap for pic

--- some_tests/main.py
+++ some_tests/main.py
@@ -1,8 +1,9 @@
 import base64
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-from test_pic_b64 import b64_encoded_pic
+from test_pic_b64 import b64_encoded_pic, b64_encoded_pic_2
 
 def is_valid_media(b64_encoded):
     b64_buf = b64_encoded # to generate byte type of instead str type => b'hello'
@@ -10,9 +11,25 @@ def is_valid_media(b64_encoded):
     # Convert image as thermal image
     # convert image to RGB matrix
     nparr = np.frombuffer(base64.b64decode(b64_buf), dtype=int)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = cv2.imdecode(nparr, cv2.COLOR_BGR2HSV)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('TEST', img)
+    #scale_percent = 100  # percent of original size
+    #width = int(img.shape[1] * scale_percent / 100)
+    #height = int(img.shape[0] * scale_percent / 100)
+
+    width = 300
+    height = 300
+    dim = (width, height)
+
+    # resize image 300 * 300
+    resized_img = cv2.resize(gray_img, dim, interpolation=cv2.INTER_AREA)
+    heatmap_img = cv2.applyColorMap(resized_img, cv2.COLORMAP_JET)
+
+
+    # https://stackoverflow.com/questions/61979855/changing-colours-of-an-area-in-an-image-using-opencv-in-python
+
+    cv2.imshow('TEST', heatmap_img)
 
     cv2.waitKey()
     return "wip"
